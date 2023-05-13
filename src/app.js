e1a6c8e8bd94597bbb28eee11a7bc2395974b549
FavourPeters <favourpeters1@gmail.com>
2023-05-13 08:40:19 +0100

    preloader

--- src/app.js
+++ src/app.js
@@ -3,21 +3,18 @@ let hamb;
 let nav;
 hamb = document.querySelector('#navi');
 let klose = document.querySelector('#close');
-// let loader;
-// loader = document.getElementById('loader');
 
-// loader.addEventListener("click", function () {
-//     pre_loader.style.display = "inline-block";
+let lod = document.querySelector('.loader');
 
-//     console.log(pre_loader);
-// })
+window.addEventListener('load', function () {
+    lod.style.display = 'none';
+})
 
-// console.log(pre_loader);
-klose.addEventListener('click', out)
+klose.addEventListener('click', out);
 
 function out() {
     nav.classList.add('translate-x-full')
-}
+};
 
 
 hamb.addEventListener('click', phnav);
@@ -26,4 +23,4 @@ function phnav() {
     nav = document.querySelector('.hide');
 
     nav.classList.remove('translate-x-full');
-}
+};
