a61501558cb41f5bd422dd3f425112d01303bacb
FavourPeters <favourpeters1@gmail.com>
2023-06-04 17:13:05 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 // UI
-let hamb;
-let nav;
-hamb = document.querySelector('#navi');
+let hamb = document.querySelector('#navi');
+let nav = document.querySelector('.hide');
 let klose = document.querySelector('#close');
 
+
+// Pre-loader
 let lod = document.querySelector('.loader');
 
 window.addEventListener('load', function () {
     lod.style.display = 'none';
 })
 
+// The nav Close
 klose.addEventListener('click', out);
 
 function out() {
     nav.classList.add('translate-x-full')
 };
 
-
+//Nav button
 hamb.addEventListener('click', phnav);
 
 function phnav() {
@@ -24,3 +26,9 @@ function phnav() {
 
     nav.classList.remove('translate-x-full');
 };
+// remove nav
+nav.addEventListener('mouseleave', rem_nav)
+
+function rem_nav() {
+    nav.classList.add('translate-x-full');
+}
